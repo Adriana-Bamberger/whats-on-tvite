16fdf9b7fead710a29687b6792298f4e779e819a
Valerian <163621755+valerian-norman@users.noreply.github.com>
2024-05-03 15:37:20 +1200

    completed lib except for stretch

--- lib.ts
+++ lib.ts
@@ -6,12 +6,12 @@ import schedule from './schedule.ts'
 import { Show } from './models/show.ts'
 
 // Complete the getTime function.
-export function getTime(show: Show) {
+export function getTime(show: Show): string {
   return show.time
 }
 
 // Complete the getName function.
-export function getName(show: Show) {
+export function getName(show: Show): string {
   return show.name
 }
 
@@ -19,21 +19,25 @@ export function getName(show: Show) {
 export function getShowTimes(): string[] {
   //make an array, push things into it from each show using .map
 
-  return schedule.map((show) => show.time)
+  return schedule.map((show) => getTime(show))
 }
 
 // Complete the getShowNames function, returning an array of show names.
-export function getShowNames() {
-  return schedule.map((show) => show.name)
+//at first we did the same as above but for the the name instead of the time.
+
+export function getShowNames(): string[] {
+  return schedule.map((show) => getName(show))
 }
+
 // Complete the getShowByTimeslot function, returning a show based on the timeslot or undefined if not found.
-export function getShowByTimeslot(timeslot: string) {
-  const placeholderShow = {
-    time: 'Placeholder time',
-    name: 'Placeholder show',
-    description: 'Placeholder description',
-  }
-  return placeholderShow
+export function getShowByTimeslot(timeslot: string): Show | undefined {
+  // const placeholderShow = {
+  //   time: 'Placeholder time',
+  //   name: 'Placeholder show',
+  //   description: 'Placeholder description',
+  // }
+
+  return schedule.find((show) => show.time === timeslot)
 }
 
 // =========
